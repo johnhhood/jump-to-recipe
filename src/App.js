e4eb627d2dbc6c7fe4d3c26c38438e7e56ede889
johnhhood <johnhood2013@gmail.com>
2025-05-02 18:35:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import RecipeList from "./components/RecipeList";
 import RecipeDetail from "./components/RecipeDetail";
+import About from "./components/About";
+import Home from "./components/Home";
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<RecipeList />} />
+       <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="recipes" element={<RecipeList />} />
           <Route path="recipe/:id" element={<RecipeDetail />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </Router>
